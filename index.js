8d9465e586e6f5126dc5f6b77b714bc693b027db
victormartinez6 <Victormartinez6@gmail.com>
2026-03-17 09:31:51 -0400

    Update index.js
    Alteracao do Spread do EURO

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const moedas = ["USD", "EUR", "GBP", "CAD", "AUD"];
 // Spread percentual por moeda
 const SPREADS = {
   USD: 0.067,
-  EUR: 0.066,
+  EUR: 0.085,
   GBP: 0.099,
   CAD: 0.064,
   AUD: 0.064,
